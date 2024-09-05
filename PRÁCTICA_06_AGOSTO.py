6b509427c8c9bd5b7cb97fec5b35d81d526c4c41
juanpamarz <jmonroy26@alumnos.uaq.mx>
2024-09-05 01:04:10 -0600

    PRIMERO

---
+++ PRÁCTICA_06_AGOSTO.py
@@ -0,0 +1,24 @@
+x = 5.26
+x = int (x)
+print(x) # Sive para darle un valor a una variable y aunque el numero no corresponda a ese tipo de dato 
+#nos lo imprima como ese tipo
+
+a = "Me gustan los arboles"
+print(a[2]) # Sirve para que nos imprima la posición a elejir de nuestra variable
+
+z = ("JuanPa")
+print(len(z)) #Sirve para dame la longitud de una cuerda
+
+d = [1, 2, 3] #Nos puede servir tanto para carecteres alfacbeticos, como numeros.
+print ( 1 in d) # Nos dice si una frase esta presente en nuestra variable y nos devuelve un True or False
+
+b = "Juan Pablo se esta preparando para su futuro"
+if "Juan Pablo" in b:# Si se cumple la condición
+    print ("Juan Pablo, está presente") #Se imprime el siguiente mensaje
+
+# Para determinar si una determinada fráse o caracter no está presente en una cadena se puede utilizar NOT IN
+q = "Pau, me está subestimando"
+if "Paula" not in q:
+    print ("Paula, está pendeja")
+
+
